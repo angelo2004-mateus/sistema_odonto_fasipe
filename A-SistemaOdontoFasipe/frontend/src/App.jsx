6ff3f9662f8859680a8f27096b5e08e0e71d5420
GabriellMiran <gabrielmiran00@gmail.com>
2024-07-06 01:02:50 -0400

    v1.2

--- A-SistemaOdontoFasipe/frontend/src/App.jsx
+++ A-SistemaOdontoFasipe/frontend/src/App.jsx
@@ -3,9 +3,10 @@ import "./App.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import CadastroPaciente from "./pages/CadastroPaciente";
-import ListarPacientes from "./pages/ListarPacientes";
 import Navbar from "./pages/Navbar";
 import Paciente from "./pages/Paciente";
+import ListarPacientes from "./pages/ListarPacientes";
+import CadastroAnamnese from "./pages/CadastroAnamnese";
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
             <Route path="/" element={<Dashboard />} />
             <Route path="/paciente/cadastro" element={<CadastroPaciente />} />
             <Route path="/paciente/deletar" element={<Paciente />} />
+            <Route path="/paciente/listarPacientes" element={<ListarPacientes />} />
+            <Route path="/paciente/cadastroAnamnese" element={<CadastroAnamnese />} />
+            
           </Routes>
         </div>
       </BrowserRouter>
